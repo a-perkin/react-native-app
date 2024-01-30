e13e6125d9008ad10e84912c6d0676f3287918b2
a-perkin <aleksandr.perkin90@gmail.com>
2024-01-30 20:25:26 +0200

    rollback-pagination

--- api/controller.tsx
+++ api/controller.tsx
@@ -1,38 +1,18 @@
 import axios from "axios";
 import { PROPERTIES } from "../properties";
 import { CARDS } from "../constants/cards";
-import { additionalPromoData3, additionalPromoData5 } from "../constants/promoData";
 
 const API_URL = "http://192.168.0.109:5003/";
-const useMock = PROPERTIES.useMock; 
+const useMock = PROPERTIES.useMock;
 
 export const getCards = async () => {
   try {
-    const response = useMock ? CARDS : (await axios.get(`${API_URL}/cards`)).data;
+    const response = useMock
+      ? CARDS
+      : (await axios.get(`${API_URL}/cards`)).data;
     return response;
   } catch (error) {
     console.log(error);
     throw error;
   }
 };
-
-export const getPromo = async () => {
-  try {
-    const response = useMock ? additionalPromoData3 : (await axios.get(`${API_URL}/promo`)).data;
-    return response;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
-
-export const getAddPromo = async () => {
-  try {
-    const response = useMock ? additionalPromoData5 : (await axios.get(`${API_URL}/add-promo`)).data;
-    return response;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
-
--- components/Promo.tsx
+++ components/Promo.tsx
@@ -4,74 +4,16 @@ import {
   Image,
   Share,
   TouchableWithoutFeedback,
-  RefreshControl,
 } from "react-native";
 import { styles } from "../constants/styles";
 import { useEffect, useRef, useState } from "react";
 import { promoData } from "../constants/promoData";
 import { COLORS } from "../constants/colors";
-import { getAddPromo, getPromo } from "../api/controller";
 
 const Promo = ({ navigation }) => {
   const [data, setData] = useState(promoData);
   const flatListRef = useRef(null);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [refreshing, setRefreshing] = useState(false);
-  const [isGetPromo3, setIsGetPromo3] = useState(false);
-  const [isGetPromo5, setIsGetPromo5] = useState(false);
-
-  //load 5 items
-  const loadAddData = () => {
-    if (refreshing) return;
-    setRefreshing(true);
-    fetchAddPromoFromApi().then((result) => {
-      if (!isGetPromo5) {
-        const newData = data.concat(result);
-        setData(newData);
-        setIsGetPromo5(true);
-      }
-      setRefreshing(false);
-    });
-  };
-
-  const fetchAddPromoFromApi = async () => {
-    try {
-      const result = await getAddPromo();
-      return result;
-    } catch (error) {
-      console.error("Error while receiving data:", error);
-    }
-  };
-
-  //refresh and load 3 items
-  const onRefresh = () => {
-    if (refreshing) return;
-    console.log("onRefresh");
-    setRefreshing(true);
-    fetchPromoFromApi().then((result) => {
-      console.log(result);
-      if (!isGetPromo3) {
-        const newData = data.concat(result);
-        setData(newData);
-        setIsGetPromo3(true);
-      }
-      setRefreshing(false);
-    });
-  };
-
-  //auto load data 3 items
-  useEffect(() => {
-    setTimeout(onRefresh, 3000);
-  }, []);
-
-  const fetchPromoFromApi = async () => {
-    try {
-      const result = await getPromo();
-      return result;
-    } catch (error) {
-      console.error("Error while receiving data:", error);
-    }
-  };
 
   //Share
   const onShare = async () => {
@@ -97,7 +39,6 @@ const Promo = ({ navigation }) => {
   useEffect(() => {
     //autoscroll
     const intervalId = setInterval(() => {
-      if (refreshing) return;
       if (currentIndex == data.length - 1) {
         flatListRef.current.scrollToIndex({
           animated: true,
@@ -160,9 +101,6 @@ const Promo = ({ navigation }) => {
               <Image source={item.img} style={styles.promoImage}></Image>
             </TouchableWithoutFeedback>
           )}
-          refreshControl={<RefreshControl refreshing={refreshing} />}
-          onEndReached={loadAddData}
-          onEndReachedThreshold={1}
         />
         {renderPagination()}
       </View>
